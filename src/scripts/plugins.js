edf1d5bf948854cd77b7ecaca5d9a6bac45c74d6
liranp <liran.polak@gmail.com>
2020-12-22 01:32:02 +0200

    fix(docsify): expand the sidebar when `?id=` is set

--- src/scripts/plugins.js
+++ src/scripts/plugins.js
@@ -118,3 +118,134 @@ const renderEditLink = (file) => {
 
   return `<div class="edit-link"><a class="github-link" onclick="editDocHandler(event)"><img class="github-icon" src="/_media/images/github.png" /> Edit on GitHub</a></div>`;
 };
+
+/**
+ * Expands the active element's subtree when `subMaxLevel=1`.
+ *
+ * Bugfix(Docsify): The `nav` map is populated with high-level links when
+ * `subMaxLevel` equals 1, so `li` element becomes undefined as `getNavKey`
+ * returns a key that contains both path and id. Therefore, the `active` class
+ * is removed from the element and the sidebar remains collapsed.
+ *
+ * Refs:
+ *  - https://github.com/docsifyjs/docsify/blob/v4.11.6/src/core/event/index.js#L15-L18
+ *  - https://github.com/docsifyjs/docsify/blob/v4.11.6/src/core/event/scroll.js#L92
+ *  - https://github.com/docsifyjs/docsify/blob/v4.11.6/src/core/event/scroll.js#L152
+ *
+ * Invoked each time after the data is fully loaded.
+ *
+ * @param hook
+ * @param vm
+ */
+export const expandActivePlugin = (hook, vm) => {
+  hook.doneEach(() => {
+    const path = vm.route.path;
+    const id = vm.route.query.id;
+    if (!id) return;
+
+    const { topMargin } = vm.config;
+    const section = document.querySelector("#" + id);
+    section && scrollTo(section, topMargin);
+
+    const nav = getSidebarLeaves(vm.router);
+    const { subMaxLevel } = vm.config;
+    const li = subMaxLevel === 1 ? nav[path] : nav[getNavKey(path, id)];
+    const sidebar = document.querySelector(".sidebar");
+    const active = sidebar.querySelector("li.active");
+    active && active.classList.remove("active");
+    li && li.classList.add("active");
+
+    expandActive(li);
+    setTimeout(() => {
+      li.classList.remove("collapse");
+    }, 0);
+  });
+};
+
+/**
+ * Expands the active element's subtree.
+ *
+ * @param node
+ */
+const expandActive = (node) => {
+  if (node) {
+    node.classList.add("open", "active");
+    while (node && node.className !== "sidebar-nav" && node.parentNode) {
+      if (node.parentNode.tagName === "LI") {
+        node.parentNode.classList.add("open");
+      }
+      node = node.parentNode;
+    }
+  }
+};
+
+/**
+ * Extracts all links from the sidebar.
+ *
+ * @param router
+ * @returns {{}}
+ */
+const getSidebarLeaves = (router) => {
+  const nav = {};
+  const sidebar = find(".sidebar");
+  let lis = [];
+  if (sidebar !== null && sidebar !== undefined) {
+    lis = findAll(sidebar, "li");
+  }
+
+  for (let i = 0, len = lis.length; i < len; i += 1) {
+    const li = lis[i];
+    const a = li.querySelector("a");
+    if (!a) {
+      continue;
+    }
+
+    let href = a.getAttribute("href");
+    if (href !== "/") {
+      const {
+        query: { id },
+        path,
+      } = router.parse(href);
+      if (id) {
+        href = getNavKey(path, id);
+      }
+    }
+
+    if (href) {
+      nav[decodeURIComponent(href)] = li;
+    }
+  }
+
+  return nav;
+};
+
+/**
+ * Finds an element.
+ *
+ * @param el
+ * @param node
+ * @returns {*}
+ */
+const find = (el, node) =>
+  node ? el.querySelector(node) : document.querySelector(el);
+
+/**
+ * Finds all elements.
+ *
+ * @param el
+ * @param node
+ * @returns {*[]}
+ */
+const findAll = (el, node) =>
+  [].slice.call(
+    node ? el.querySelectorAll(node) : document.querySelectorAll(el)
+  );
+
+/**
+ * Returns a unique key by concatenating the path and the id.
+ *
+ * @param path
+ * @param id
+ * @returns {string}
+ */
+const getNavKey = (path, id) => `${path}?id=${id}`;
