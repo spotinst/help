528cc8a04198c27c0f4f63d5ca8f01a7b5db3eb8
liranp <liran.polak@gmail.com>
2021-02-18 22:20:01 +0000

    Deploying to gh-pages from  @ ba44f9f015d97a10bb0f2cbfdc608c3b1202bd51 🚀

--- bundle.js
+++ bundle.js
@@ -157,9 +157,11 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	})();
 /******/ 	
 /************************************************************************/
+/******/ 	
 /******/ 	// startup
-/******/ 	// Load entry module
-/******/ 	__webpack_require__("./src/index.js");
-/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
 /******/ })()
 ;
