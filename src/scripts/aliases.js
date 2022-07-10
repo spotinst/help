f54d9f76fcae27092f652c3ed77b74814699565d
netapp-jmontel <jmontel@netapp.com>
2022-07-10 13:54:04 +0300

    Update aliases.js

--- src/scripts/aliases.js
+++ src/scripts/aliases.js
@@ -1651,6 +1651,9 @@ export const aliases = (() => {
       "/ocean/getting-started/eks/",
 
     "/cloud-analyzer/tutorials/manage-subsets": "/eco/tutorials/manage-subsets",
+
+    "/elastigroup-for-azure/shutdown-script-in-elastigroup/":
+      "/elastigroup/features-azure/shutdown-script-in-elastigroup-for-azure?id=shutdown-script-in-elastigroup-for-azure",  
   };
 
   for (let [key, value] of Object.entries(map)) {
