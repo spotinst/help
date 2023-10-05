eccf6bcfb1e5f4216d2a7cc05830c8daf4bf978d
Elinor <106514736+elinornetapp@users.noreply.github.com>
2023-10-05 14:41:34 +0300

    Update aliases.js

--- src/scripts/aliases.js
+++ src/scripts/aliases.js
@@ -1783,7 +1783,7 @@ export const aliases = (() => {
     "/eco/getting-started/connect-azure-ea-to-eco":
       "eco/getting-started/connect-azure-account",
 
-    "/ocean/getting-started/aks": "ocean/getting-started/aks/",    
+    "/ocean/getting-started/aks": "ocean/getting-started/aks/",
   };
 
   for (let [key, value] of Object.entries(map)) {
