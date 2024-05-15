81d0808ce1aa7bbc67a5210e1b2276eb49720443
rickshar <159915991+rickshar@users.noreply.github.com>
2024-05-15 23:19:43 +0300

    Update aliases.js

--- src/scripts/aliases.js
+++ src/scripts/aliases.js
@@ -1800,7 +1800,7 @@ export const aliases = (() => {
       "elastigroup/tutorials/elastigroup-actions-menu/",
 
     "/ocean/tutorials/connect-an-aks-private-cluster":
-      "ocean/getting-started/aks/what-to-do-about-aks-private-clusters",
+      "ocean/getting-started/aks/import-an-aks-cluster-to-ocean",
   };
 
   for (let [key, value] of Object.entries(map)) {
